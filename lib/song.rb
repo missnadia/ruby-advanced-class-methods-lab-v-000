f2b6cff56c1de8a8ea9ab08fd1c7f29def8dae52
Nadia Lee <miss.nadia.lee@gmail.com>
2018-01-19 03:50:56 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,15 +16,42 @@ class Song
     @@all << song
   end
 
-  def new_by_name
+  def self.new_by_name(name)
 
   end
 
-  def create_by_name
+  def self.create_by_name(name)
 
   end
 
   def self.find_by_name(name)
     self.all.detect {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+  end
+
+  def self.alphabetical(name)
+
+  end
+
+  def self.new_from_filename(name)
+    artist_song = name.collect do |titles|
+      data = titles.split(" - ")
+      artist = data[0]
+      title = data[2]
+      song = self.new
+      song.artist = artist
+      song.title = title
+      song
+    end
+    artist_song
+  end
+
+  def self.create_from_filename(name)
+  end
+
+  def self.destroy_all
+    @@all.clear
   end
 end
