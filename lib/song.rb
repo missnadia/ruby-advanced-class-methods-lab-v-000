4f4d6223fef23d45f0f2406c7801fb35e1dcdd0c
Nadia Lee <miss.nadia.lee@gmail.com>
2018-01-19 04:01:06 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,59 +11,60 @@ class Song
     self.class.all << self
   end
 
-  def self.create(name)
-    song = self.new
+  def self.create
+    song = Song.new
     song.save
     song
   end
 
-  def self.new_by_name(name)
-    song = Song.new
-    song.name = name
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
     song
   end
 
-  def self.create_by_name(name)
+  def self.create_by_name(song_name)
     song = self.create
-    song.name = name
+    song.name = song_name
     song
   end
 
-  def self.find_by_name(name)
-    self.all.detect {|song| song.name == name}
+  def self.find_by_name(song_name)
+    self.all.detect{|s| s.name == song_name}
   end
 
-  def self.find_or_create_by_name(name)
-    self.find_by_name(name) || self.create_by_name(name)
+  def self.find_or_create_by_name(song_name)
+    self.find_by_name(song_name) || self.create_by_name(song_name)
   end
 
-  def self.alphabetical(name)
-    self.all.sort_by{|song| song.name}
+  def self.alphabetical
+    self.all.sort_by{|s| s.name}
   end
 
   def self.new_from_filename(filename)
-     parts = filename.split(" - ")
-     artist_name = parts[0]
-     song_name = parts[1].gsub(".mp3", "")
+    parts = filename.split(" - ")
+    artist_name = parts[0]
+    song_name = parts[1].gsub(".mp3", "")
 
-     song = self.new
-     song.name = song_name
-     song.artist_name = artist_name
-     song
-   end
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
 
-   def self.create_from_filename(filename)
-     parts = filename.split(" - ")
-     artist_name = parts[0]
-     song_name = parts[1].gsub(".mp3", "")
+  def self.create_from_filename(filename)
+    parts = filename.split(" - ")
+    artist_name = parts[0]
+    song_name = parts[1].gsub(".mp3", "")
 
-     song = self.create
-     song.name = song_name
-     song.artist_name = artist_name
-     song
- end
+    song = self.create
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
 
   def self.destroy_all
     self.all.clear
   end
+
 end
