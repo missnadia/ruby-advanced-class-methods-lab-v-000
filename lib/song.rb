d3512afae464507eed2e86ba34d5ab0a7462b431
Nadia Lee <miss.nadia.lee@gmail.com>
2018-01-19 04:00:30 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,26 +41,26 @@ class Song
   end
 
   def self.new_from_filename(filename)
-    artist_song = filename.split(" - ")
-    artist = artist_song[0]
-    title = artist_song[1].gsub(".mp3", "")
+     parts = filename.split(" - ")
+     artist_name = parts[0]
+     song_name = parts[1].gsub(".mp3", "")
 
-    song = self.new
-    song.artist = artist
-    song.title = title
-    song
-  end
+     song = self.new
+     song.name = song_name
+     song.artist_name = artist_name
+     song
+   end
 
-  def self.create_from_filename(filename)
-    artist_song = filename.split(" - ")
-    artist = artist_song[0]
-    title = artist_song[1].gsub(".mp3", "")
+   def self.create_from_filename(filename)
+     parts = filename.split(" - ")
+     artist_name = parts[0]
+     song_name = parts[1].gsub(".mp3", "")
 
-    song = self.new
-    song.artist = artist
-    song.title = title
-    song
-  end
+     song = self.create
+     song.name = song_name
+     song.artist_name = artist_name
+     song
+ end
 
   def self.destroy_all
     self.all.clear
