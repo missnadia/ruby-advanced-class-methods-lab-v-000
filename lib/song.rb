de27e7a9e266e3970fbb6e3d65726a49a4b214ec
Nadia Lee <miss.nadia.lee@gmail.com>
2018-01-19 03:38:56 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,21 @@ class Song
     self.class.all << self
   end
 
+  def self.create(name)
+    song = self.new
+    song.name = name
+    @@all << song
+  end
+
+  def new_by_name
+
+  end
+
+  def create_by_name
+
+  end
+
+  def self.find_by_name(name)
+    self.all.detect {|song| song.name == name}
+  end
 end
