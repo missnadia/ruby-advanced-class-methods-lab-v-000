3fc090965bcf0c6d2ab8f10b1c28321f76d5aa72
Nadia Lee <miss.nadia.lee@gmail.com>
2018-01-19 03:53:07 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,12 +12,14 @@ class Song
 
   def self.create(name)
     song = self.new
-    song.name = name
-    @@all << song
+    song.save
+    song
   end
 
   def self.new_by_name(name)
-
+    song = self.new_by_name
+    song.name = name
+    song
   end
 
   def self.create_by_name(name)
